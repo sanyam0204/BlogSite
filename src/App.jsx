93648ac4171d78db5d2eb676ee20bacec420ffa8
sanyam0204 <sanyam0204@gmail.com>
2024-05-25 00:19:04 +0530

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import conf from "./conf/conf";
+
+function App() {
+  console.log(conf.appwriteBucketId);
+  return (
+    <>
+      <h1>Blog Site</h1>
+    </>
+  );
+}
+
+export default App;
