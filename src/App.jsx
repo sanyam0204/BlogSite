36f08b45706c29efdb562b35ac751a0b562e9551
sanyam0204 <sanyam0204@gmail.com>
2024-05-26 23:03:52 +0530

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import "./App.css";
-import conf from "./conf/conf";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  console.log(conf.appwriteBucketId);
-  return (
-    <>
-      <h1>Blog Site</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
